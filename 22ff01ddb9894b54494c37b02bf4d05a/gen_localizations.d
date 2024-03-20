78a62e8628c631d32aef6253c8b7ec3f0e4db446
ecompagno <edoardo.compagno@soprasteria.com>
2024-03-20 18:57:30 +0100

    added assets

---
+++ 22ff01ddb9894b54494c37b02bf4d05a/gen_localizations.d
@@ -0,0 +1,1 @@
+ /Users/edoardocompagno/Documents/repos/wedding-site/.dart_tool/flutter_gen/gen_l10n/app_localizations_it.dart /Users/edoardocompagno/Documents/repos/wedding-site/.dart_tool/flutter_gen/gen_l10n/app_localizations.dart:  /Users/edoardocompagno/Documents/repos/wedding-site/l10n.yaml /Users/edoardocompagno/Documents/repos/wedding-site/assets/locales/app_it.arb
